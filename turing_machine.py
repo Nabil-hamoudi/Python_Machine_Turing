19246c35fa7a04ea471696c0ef38a69799900280
Nabil-hamoudi <nabil.hamoudi@ens.uvsq.fr>
2022-12-01 16:51:10 +0100

    Fonction de la Classe TuringMachineCode pour ajouter des objet fini

--- turing_machine.py
+++ turing_machine.py
@@ -4,7 +4,7 @@ DEPLACEMENT = {0: "Stay", 1: "Left", 2: "Right"}
 class TuringMachineCode:
     # initialise la machine de turing en prenant pour entrée
     # le nombre de ruban qui doit etre au moins de 1,
-    # les transition [((q1,q2), [((value1, value2), DEPLACEMENT)...])...],
+    # les transition [[q1,q2], [[[value1, value2], DEPLACEMENT]...]],
     # l'état initial et l'état final,
     # Les états doivent étre numèroté et un dictionnaire doit etre donnée dans
     # etat avec leur noms
@@ -12,13 +12,20 @@ class TuringMachineCode:
     def __init__(self, name):
         self.name = name
 
-        self.final = []
+        self.init = None
+        self.final = None
+        self.etat_nombre = 0
         self.etat = {}
         self.etat_transi = []
         self.value = []
         self.deplacement = []
         self.ruban = []
         self.tete = []
+
+    def ajout_etat(self, etat):
+        self.etat[self.etat_nombre] = etat
+        self.etat_nombre += 1
+        return self.etat_nombre - 1
 
     def init_ruban(self, nombre_ruban):
         if len(self.ruban) == 0:
@@ -30,32 +37,62 @@ class TuringMachineCode:
 
     def init_etat_initial(self, init):
         if self.init is None:
-            self.init = init
+            inside = False
+            for j in self.etat.keys():
+                if init in self.etat[j]:
+                    inside = True
+                    self.init = j
+            if not inside:
+                self.init = self.ajout_etat(init)
         else:
             print("Init initialiser 2 fois")
 
     def init_final(self, final):
-        self.final.append(final)
+        if self.final is None:
+            inside = False
+            for j in self.etat.keys():
+                if final in self.etat[j]:
+                    inside = True
+                    self.final = j
+            if not inside:
+                self.final = self.ajout_etat(final)
+        else:
+            print("Accept initialiser 2 fois")
 
     def init_transition_ruban(self, transition):
-        self.etat_transi.append(transition[0])
+        couple = []
+        for i in transition[0]:
+            inside = False
+            for j in self.etat.keys():
+                if i == self.etat[j]:
+                    inside = True
+                    couple.append(j)
+            if not inside:
+                couple.append(self.ajout_etat(i))
+
+        self.etat_transi.append(couple)
         self.value.append([])
         self.deplacement.append([])
-
-        for j in transition[1]:
-            self.value[-1].append(j[0])
-            self.deplacement[-1].append(j[1])
+        self.value[-1].append(transition[1][0])
+        self.deplacement[-1].append(transition[1][1])
 
         self.init_ruban(len(transition[1]))
 
     def machine_turing_integrity(self):
         if self.init is None:
             print("No init define")
+            return False
+        if self.final is None:
+            print("No accept state define")
 
 
-
-class InstanceMachineTuring(TuringMachineCode):
-    # initialise une instance d'une
-    # machine de turing défini précédement
-    def __init__(self, path, word):
-        pass
+if __name__ == '__main__':
+    machine = TuringMachineCode("test")
+    machine.init_etat_initial("init")
+    machine.init_etat_initial("init2")
+    machine.init_final("accept")
+    machine.init_final("accept2")
+    machine.init_transition_ruban([["q0", "q1"], [["1", "2"], "Right"]])
+    print(machine.etat)
+    print(machine.ruban, machine.etat_transi, machine.value, machine.tete)
+    print(machine.etat_nombre, machine.value, machine.deplacement)
